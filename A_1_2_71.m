f6fb5c5c9e32a0f605ffeb1e49f6561ccaf0cc4c
Hannes F <62026775+afy@users.noreply.github.com>
2021-03-15 19:05:32 +0100

    Done

--- A_1_2_71.m
+++ A_1_2_71.m
@@ -1,14 +1,21 @@
 format long % enable 4 decimal place precision
+
+% plot 
 f = @(x) (sin(sqrt(1-x))) ./ (sqrt(1-(x.^2))); % "sin" is radians by default
+hold on
 fplot(f);
+plot(0.999, f(0.999), 'r*') % read x value from graph plot
+hold off
 
-% adjust zoom and read result graphically
+% print result
+fprintf("when x->1-  then y reaches %f", f(0.999))
+
+% style
 grid on
-axis([-5 5 -5 5])           
-axis([-1 1 0.6 1])
-axis([-0.5 0.5 0.82 0.86])
-
-legend("(sin(sqrt(1-x))) ./ (sqrt(1-(x.^2)))")
+axis([-0.2 1.5 -0.2 1.5])
+legend("(sin(sqrt(1-x))) ./ (sqrt(1-(x.^2)))", "approx. limit")
 title("Block B: 1.2.71")
+line(xlim, [0, 0])
+line([0, 0], ylim)
 xlabel("x-axis")
 ylabel("y-label")
