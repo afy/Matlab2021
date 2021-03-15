e964b189461473c19ea7fd9ac19c30b8dd4b0c88
Hannes F <62026775+afy@users.noreply.github.com>
2021-03-15 19:27:44 +0100

    Done

--- A_2_8_24.m
+++ A_2_8_24.m
@@ -5,23 +5,25 @@ f = @(x) (2*x+1) ./ ((x.^2) + x + 1);
 g = @(x) -(f(x)); % inverse f
 fplot(f)
 grid on
-axis([-50 50 -2 2]) % assuming trend continues, critical points ( f'(x)=0 )
-% should all be in the open interval (-50, 50)
+axis([-14 14 -2 2]) % assuming trend continues, critical points ( f'(x)=0 )
+% should all be in the open interval (-14, 14)
 
 % calculate critical points
 % function has a minimum and a maximum value, the 2 critical points
-fmin = fminbnd(f, -50, 50);
-fmax = fminbnd(g, -50, 50);
+bnd = 14;
+fmin = fminbnd(f, -bnd, bnd);
+fmax = fminbnd(g, -bnd, bnd);
+fprintf("Min: [%f, %f]\n", fmin, f(fmin))
+fprintf("Max: [%f, %f]\n", fmax, f(fmax))
 
 % mark out critical points
 hold on
-fplot(f, [-50, 50])
 plot(fmin, f(fmin), 'r*')
 plot(fmax, f(fmax), 'r*')
 hold off
 
 % style
 title("Block B: 2.8.24")
-legend("(2*x+1) / ((x.^2) + x + 1)")
+legend("(2*x+1) / ((x.^2) + x + 1)", "min", "max")
 xlabel("x-axis")
 ylabel("y-axis")
