cc85edae8bc7df525a0af44d3f69018d5ea37fc6
Hannes F <62026775+afy@users.noreply.github.com>
2021-03-15 19:17:55 +0100

    Done

--- uppgift_2_1_30.m
+++ uppgift_2_1_30.m
@@ -1,20 +1,16 @@
-title('Block 2 2.1.30') %title
-xlabel('x-axis') %x title
-ylabel('y-axis') %y title
-axis([-4 4 -2 6]) %zoom factor
-xL = xlim;        %limit for lines, see next comment
-yL = ylim;
-line ([0 0], yL); %draw x-axis
-line (xL, [0 0]); %draw y-axis
-line ([-1 1],[1 1],'color','r') %draw tangent from x=-1 to x=1 on y=1
-
+% plot function
 hold on
 grid on %draw grid
-
 f = @(x) ((x.^2-1).^2).^(1./3);
 fplot(f)
-%draw equation
-
+line ([-1 1],[1 1],'color','r') %draw tangent from x=-1 to x=1 on y=1
 hold off
 
-
+% style
+title('Block B: 2.1.30') 
+legend("((x^2-1)^2)^(1/3)", "horizontal tangent at [0, 1]")
+xlabel('x-axis')
+ylabel('y-axis')
+axis([-4 4 -2 6]) %zoom factor
+line ([0 0], ylim); %draw x-axis
+line (xlim, [0 0]); %draw y-axis
