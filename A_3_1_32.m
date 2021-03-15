610a07dc9f2dbe7735afdde33ffec09369d07ccf
Hannes F <62026775+afy@users.noreply.github.com>
2021-03-15 19:48:35 +0100

    Done

--- A_3_1_32.m
+++ A_3_1_32.m
@@ -4,9 +4,15 @@ format long
 % =>  g^-1(x) = y = where g(y) = 2
 % y^-1' = 1 / g'(y)
 
-% calculate
+% calculate g^-1(x)=2
 g = @(y) 2*y + sin(y); % increases; one-to-one; invertable
-gp = @(y) y
+% g(y) = 2 => g(y) - 2 = 0  =>  2y + siny - 2 = 0
+g2 = @(x) g(x) - 2;
+z = fzero(g2, 1);
+fprintf("g^-1(x)=2 when y=%f\n", z)
 
-%plot
-
+% calculate derivative of g^-1(2)
+% ... = 1/g'(y)
+gp = @(y) 2 + cos(y); % derivative of g
+dv = 1 / (gp(z));
+fprintf("derivative at y=2 is %f\n", dv)
