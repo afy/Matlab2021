5356b05e0e5e08f31a10936c8fcee110f21d86b6
Hannes F <62026775+afy@users.noreply.github.com>
2021-03-15 19:55:36 +0100

    Done

--- uppgift_3_2_22.m
+++ uppgift_3_2_22.m
@@ -1,19 +1,5 @@
-title('Block 3 3.2.22')
-xlabel('x-axis')
-ylabel('y-axis')
-axis([-10 10 -2 4])
-xL = xlim;
-yL = ylim;
-line ([0 0], yL); %draw x-axis
-line (xL, [0 0]); %draw y-axis
-
-hold on
-grid on
-
-x = 10.^((log10(3))./(sqrt(2)));
-fplot(x)
-fprintf('%d', x)
+% x^sqrt(2) = 3  =>  sqrt(2)*log_10*x = log_10 * 3 
+% => x = 10^(log_10*3 / sqrt*2)
+x = 10.^(log10(3)./sqrt(2));
+fprintf('Value is %d', x)
 %prints answer: x = 2.174581
-plot(x, 'r*')
-
-hold off
