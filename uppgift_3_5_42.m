5b9b58c91051a413865e4c802472843c13de936e
Nearyn-hub <78608631+Nearyn-hub@users.noreply.github.com>
2021-03-15 19:55:33 +0100

    Add files via upload

---
+++ uppgift_3_5_42.m
@@ -0,0 +1,15 @@
+title('Block B 3.5.42')
+xlabel(x-axis)
+ylabel(y-axis)
+axis([-10 10 -10 10])
+xL = xlim;
+yL = ylim;
+line([0 0], yL)
+line(xL, [0 0])
+
+hold on
+grid on
+
+%sida 200 i A och 97 i lösning
+
+hold off
