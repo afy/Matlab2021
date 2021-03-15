799829e8230ba6079eda5a50cd94fef4a54ce1eb
Hannes F <62026775+afy@users.noreply.github.com>
2021-03-15 17:55:06 +0100

    Done

--- A_P4_52.m
+++ A_P4_52.m
@@ -1,16 +1,17 @@
 % plot function
-%f = @(x) (2*x^2 + 3*x) / (x^2 + 4*x + 5); non vectorized
 f = @(x) (2*(x.^2) + 3*x) ./ (x.^2 + 4*x + 5);
 hold on
 fplot(f)
 plot(-2, f(-2), 'r*') % origin
+line(xlim, [0, 0])
+line([0, 0], ylim)
 hold off
 
 
 % style plot
 grid on
 axis([-10 10 -4 8])
-legend("(2*x^2 + 3*x) / (x^2 + 4*x + 5)", "origo");
-title("Block A P4.52")
+legend("(2*(x.^2) + 3*x) ./ (x.^2 + 4*x + 5)", "origin");
+title("Block A: P4.52")
 xlabel("x-axis")
 ylabel("y-axis")
